b54401e95f89b52f68825aa56e8838a2b4c8f24f
patricksimpson <izerop@gmail.com>
2017-11-27 12:04:07 -0500

    fix: about

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,14 +3,14 @@ import Link from 'gatsby-link';
 
 const About = () => (
   <div>
-    <h1>Hey, I am Patrick! </h1>
+    <h1>Hey, I'm Patrick! </h1>
     <h2>Perhaps the coolest guy you'll ever encounter.</h2>
     <p>
       I'm a full-stack web developer from Ohio. I make the Internet at <a href="https://twitter.com/hearsparkbox">@hearsparkbox</a>.
-       I'll be posting about developer things, but primarily, JavaScript, Emacs, and my life with four kids.
+      I'll be posting about developer things, primarily JavaScript, Emacs, and my life with four kids.
     </p>
     <p>If you wish, drop me a line on Twitter: <a href="https://twitter.com/patrexsimpson">@patrexsimpson</a></p>
-    <Link to="/">Post Listing</Link>
+    <Link to="/">Back to posts</Link>
   </div>
 );
 
