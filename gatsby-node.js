aac1a30e15330d6b49235f0d558e55bdb5b4bded
Patrick <izerop@gmail.com>
2017-11-27 23:00:09 -0500

    feat: port in old posts, publish

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,7 @@ const { createFilePath } = require(`gatsby-source-filesystem`);
 exports.onCreateNode = ({ node, getNode, basePath, boundActionCreators }) => {
   const { createNodeField } = boundActionCreators
   if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    const slug = createFilePath({ node, getNode, basePath: `posts` })
     createNodeField({
       node,
       name: `slug`,
--- src/templates/post.js
+++ src/templates/post.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Link from "gatsby-link";
-import g from "glamorous";
 
 export default ({ data }) => {
   const post = data.markdownRemark
   return (
     <div>
-      <g.H1>
+      <h1>
         {post.frontmatter.title}
-      </g.H1>
+      </h1>
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
       <Link to="/">Back to posts</Link>
     </div>
