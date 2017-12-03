930b32168f5d1f0a4ece5badb5c8e84cad888789
patricksimpson <izerop@gmail.com>
2017-12-02 23:47:56 -0500

    feat: auto link headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
+          `gatsby-remark-autolink-headers`,
           {
             resolve: `gatsby-remark-prismjs`,
             options: {
