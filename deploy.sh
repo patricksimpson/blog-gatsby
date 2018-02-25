5396f99004ed0a0be9fca3bb4b9b4a489b4c1d57
Patrick Simpson <izerop@gmail.com>
2018-02-25 16:34:45 -0500

    feat: added ssh -qtt to deploy

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ scp $archive_path $host:$remote_path
 
 echo "-------------> Deploying."
 
-ssh $host <<EOF
+ssh -t -t $host <<EOF
 tar -xvjf $remote_path/$archive -C $remote_path && mv $remote_path/public $remote_path/$build_name
 rm $remote_path/current
 ln -s $remote_path/$build_name $remote_path/current
